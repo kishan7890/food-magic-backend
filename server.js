9b9fee415aed6cafe833ba69b95e9974eb0237ba
kishan7890 <kishansahu9348@gmail.com>
2026-05-07 20:49:49 +0530

    frotend url added to cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://food-magic-frontend.vercel.app/",
   credentials: true
 }))
 app.use(cookieparser());
